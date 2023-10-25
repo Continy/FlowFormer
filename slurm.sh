743e8603e9831dfc1c151327d9e8bf7daf163431
Continy <satanama.ring@gmail.com>
2023-10-26 01:56:20 +0800

    upload nvidia

--- slurm.sh
+++ slurm.sh
@@ -16,4 +16,4 @@
 # Executable
 EXE=/bin/bash
 
-singularity exec --bind /data2/datasets/wenshanw/tartan_data:/zihao/datasets:ro,/data2/datasets/yuhengq/zihao/FlowFormer:/zihao/FlowFormer /data2/datasets/yuhengq/zihao/flowformer.sif bash /zihao/FlowFormer/script.sh
+singularity exec --nv --bind /data2/datasets/wenshanw/tartan_data:/zihao/datasets:ro,/data2/datasets/yuhengq/zihao/FlowFormer:/zihao/FlowFormer /data2/datasets/yuhengq/zihao/flowformer.sif bash /zihao/FlowFormer/script.sh
